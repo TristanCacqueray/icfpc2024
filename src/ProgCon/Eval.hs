a28d04bd16f9d57df27470b80e30f02bd267ac77
Tristan Cacqueray <tdecacqu@redhat.com>
2024-06-28 11:18:59 -0400

    Handle elam body

--- src/ProgCon/Eval.hs
+++ src/ProgCon/Eval.hs
@@ -1,30 +1,32 @@
 module ProgCon.Eval where
 
-import RIO
-import ProgCon.Parser
 import Data.Map.Strict qualified as Map
+import ProgCon.Parser
+import RIO
 
 type Env = Map Natural Expr
 
 evalExpr :: Env -> Expr -> Either String Expr
 evalExpr env expr = case expr of
-  EUnary UNeg (EInt x) -> pure $ EInt (negate x)
-  EUnary UNeg e -> Left $ "Bad uneg: " <> show e
-  EUnary UNot (EBool x) -> pure $ EBool (not x)
-  EUnary UNot e -> Left $ "Bad unot: " <> show e
-  EUnary Ustr2int (EStr x) -> pure $ EInt $ str2int x
-  EUnary Uint2str (EInt x) -> pure $ EStr $ int2str x
-  EUnary _ _ -> Left $ "Bad unary: " <> show expr
-  EBinary '+' (EInt x) (EInt y) -> pure $ EInt (x + y)
-  EBinary '$' e1 e2 -> case evalExpr env e1 of
-    Right (ELam var body) -> evalExpr (Map.insert var e2 env) body
-    _ -> Left $ "Bad apply op: " <> show expr
-  EBinary _ _ _ -> Left $ "TODO: " <> show expr
-  EVar v -> case Map.lookup v env of
-    Nothing -> Left $ "Unbound var: " <> show v <> ", in " <> show env <> ", for " <> show expr
-    Just e -> pure e
-  e -> pure e
-
+    EUnary UNeg (EInt x) -> pure $ EInt (negate x)
+    EUnary UNeg e -> Left $ "Bad uneg: " <> show e
+    EUnary UNot (EBool x) -> pure $ EBool (not x)
+    EUnary UNot e -> Left $ "Bad unot: " <> show e
+    EUnary Ustr2int (EStr x) -> pure $ EInt $ str2int x
+    EUnary Uint2str (EInt x) -> pure $ EStr $ int2str x
+    EUnary _ _ -> Left $ "Bad unary: " <> show expr
+    EBinary '+' (EInt x) (EInt y) -> pure $ EInt (x + y)
+    EBinary '$' (ELam var body) e2 -> evalExpr (Map.insert var e2 env) body
+    EBinary '$' e1 e2 -> case evalExpr env e1 of
+        Right (ELam var body) -> evalExpr (Map.insert var e2 env) body
+        _ -> Left $ "Bad apply op: " <> show expr
+    EBinary '.' (EStr s1) (EStr s2) -> pure $ EStr (s1 <> s2)
+    EBinary _ _ _ -> Left $ "TODO: " <> show expr
+    EVar v -> case Map.lookup v env of
+        Nothing -> Left $ "Unbound var: " <> show v <> ", in " <> show env <> ", for " <> show expr
+        Just e -> pure e
+    ELam v b -> ELam v <$> evalExpr env b
+    e -> pure e
 
 str2int :: Text -> Integer
 str2int = undefined
