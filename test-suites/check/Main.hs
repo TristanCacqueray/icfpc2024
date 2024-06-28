d8fda039d9d7250d8a53eb3093ce426941fcfeef
Ignat Insarov <kindaro@gmail.com>
2024-06-29 02:17:51 +0800

    Add more checks.

--- test-suites/check/Main.hs
+++ test-suites/check/Main.hs
@@ -2,9 +2,13 @@ module Main where
 
 import RIO
 import RIO.ByteString.Lazy qualified as Bytes
+import RIO.Char
+import RIO.Text qualified as Text
 
 import Control.Monad.Trans.Writer.CPS
+import Data.Coerce
 import System.FilePath
+import Test.QuickCheck.Instances.Text ()
 import Test.Tasty
 import Test.Tasty.Golden
 import Test.Tasty.QuickCheck
@@ -21,12 +25,24 @@ main = writerMain do
         checkCommunicationBytes "get index"
     testWriter "parser" do
       writeProperty "string" do parseExpr "S'%4}).$%8" === Right (EStr "get index")
+      writeProperty "two abstractions and two applications" do
+        parseExpr "B$ B$ L\" B$ L# B$ v\" B$ v# v# L# B$ v\" B$ v# v# L\" L# ? B= v# I! I\" B$ L$ B+ B$ v\" v$ B$ v\" v$ B- v# I\" I%"
+          === Right (EBinary '$' (EBinary '$' (ELam 1 (EBinary '$' (ELam 2 (EBinary '$' (EVar 1) (EBinary '$' (EVar 2) (EVar 2)))) (ELam 2 (EBinary '$' (EVar 1) (EBinary '$' (EVar 2) (EVar 2)))))) (ELam 1 (ELam 2 (EIf (EBinary '=' (EVar 2) (EInt 0)) (EInt 1) (EBinary '$' (ELam 3 (EBinary '+' (EBinary '$' (EVar 1) (EVar 3)) (EBinary '$' (EVar 1) (EVar 3)))) (EBinary '-' (EVar 2) (EInt 1))))))) (EInt 4))
+      writeProperty "decodeString ∘ encodeString = id" \(AppropriateText text) -> (decodeString . encodeString) text === text
+      writeProperty "encodeString ∘ decodeString = id" \(AppropriateText text) -> (encodeString . decodeString) text === text
     testWriter "evaluator" do
       writeProperty "hello" do
         evalExpr
           emptyEnvironment
           (EBinary '$' (EBinary '$' (ELam 2 (ELam 3 (EVar 2))) (EBinary '.' (EStr "Hello") (EStr " World!"))) (EInt 42))
           === Right (EStr "Hello World!")
+
+newtype AppropriateText = AppropriateText Text deriving newtype (Show, Eq, Ord)
+instance Arbitrary AppropriateText where
+  arbitrary = do
+    text <- arbitrary
+    pure do AppropriateText (Text.filter (\character -> 33 <= ord character && ord character <= 126) text)
+  shrink = coerce (shrink :: Text -> [Text])
 
 checkCommunicationBytes :: TestName -> TestTree
 checkCommunicationBytes path = goldenVsString
