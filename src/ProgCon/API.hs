279c4a8a47722e760232f4cc3643df33959d6937
Ignat Insarov <kindaro@gmail.com>
2024-06-29 04:24:30 +0800

    Add rate limiting to API requests.

--- src/ProgCon/API.hs
+++ src/ProgCon/API.hs
@@ -3,23 +3,47 @@ module ProgCon.API where
 import RIO
 import RIO.ByteString (toStrict)
 import RIO.ByteString.Lazy qualified as Lazy
+import RIO.FilePath
+import RIO.List qualified as List
 import RIO.Text qualified as Text
 
 import Data.Text.Encoding (decodeUtf8)
+import Data.Time
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
-import System.Environment
-
 import Network.HTTP.Types (Status (..))
+import System.Environment
+import System.IO.Error
+import System.IO.Temp
 
 apiServer :: String
 apiServer = "https://boundvariable.space/communicate"
 
+rateLimited :: IO anything -> IO anything
+rateLimited action = do
+  debug <- lookupEnv "ICFP_DEBUG"
+  temporaryDirectory <- getCanonicalTemporaryDirectory
+  let timersFile = temporaryDirectory </> "icfp-timers"
+  currentTime <- getCurrentTime
+  timers <- fmap (fmap (read @UTCTime) . lines) do
+    readFile timersFile
+      `catch` \exception -> if doesNotExistErrorType == ioeGetErrorType exception then pure [] else throwIO exception
+  let (expiredTimers, runningTimers) = List.partition (\timer -> currentTime `diffUTCTime` timer > 60) timers
+  if length runningTimers > 18
+    then do
+      when (isJust debug) do
+        (putStrLn . unwords) ["expired timers", show expiredTimers, "waiting for timers", show runningTimers]
+      threadDelay 1000000
+      rateLimited action
+    else do
+      writeFile timersFile ((unlines . fmap show) (currentTime : runningTimers))
+      action
+
 postBytes :: Lazy.ByteString -> IO Lazy.ByteString
-postBytes body = do
+postBytes body = rateLimited do
+  debug <- lookupEnv "ICFP_DEBUG"
   manager <- newTlsManager
   token <- getEnv "ICFP_TOKEN"
-  debug <- lookupEnv "ICFP_DEBUG"
   initialRequest <- parseRequest apiServer
   let request =
         initialRequest
