740e5436a3298a6e01c06464c2838abf3c2611b4
Tristan Cacqueray <tdecacqu@redhat.com>
2024-06-28 08:48:51 -0400

    Add string parser

--- src/ProgCon.hs
+++ src/ProgCon.hs
@@ -4,6 +4,7 @@ import Data.Text qualified as T
 import RIO
 import SimpleCmdArgs
 import ProgCon.API qualified as API
+import ProgCon.Parser qualified as Parser
 
 main :: IO ()
 main = do
@@ -16,9 +17,17 @@ mainMain =
   subcommands
   [ Subcommand "communicate" "send a message" $
     mainCommunicate
+    <$> (T.pack <$> strArg "MESSAGE"),
+    Subcommand "parse" "parse a message" $
+    mainParse
     <$> (T.pack <$> strArg "MESSAGE")
+
   ]
 
 mainCommunicate :: Text -> IO ()
 mainCommunicate message = do
   print =<< API.communicate message
+
+mainParse :: Text -> IO ()
+mainParse message = do
+  print $ Parser.parseExpr message
--- src/ProgCon/Parser.hs
+++ src/ProgCon/Parser.hs
@@ -3,3 +3,28 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module ProgCon.Parser where
+
+import Data.Attoparsec.Text qualified as P
+import Data.Text qualified as T
+import RIO
+
+data Expr
+    = EBool Bool
+    | EInt Natural
+    | EStr Text
+    deriving (Show)
+
+parseExpr :: Text -> Either String Expr
+parseExpr = P.parseOnly exprP
+
+exprP :: P.Parser Expr
+exprP = taggedP 'S' EStr stringP
+  where
+    taggedP c tag p = P.char c *> (tag <$> p)
+
+stringP :: P.Parser Text
+stringP = T.map convertChars <$> takeString
+  where
+    takeString = P.takeWhile (\c -> c >= '!' && c <= '~')
+    convertChars c = order !! (fromEnum c - 33)
+    order = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n"
