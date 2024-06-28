fb774984d48c4a27dbf0d321c0e1b4806107c5e9
Tristan Cacqueray <tdecacqu@redhat.com>
2024-06-28 09:08:24 -0400

    Add query command

--- src/ProgCon.hs
+++ src/ProgCon.hs
@@ -1,33 +1,49 @@
 module ProgCon (main) where
 
 import Data.Text qualified as T
-import RIO
-import SimpleCmdArgs
+import Data.Text.IO qualified as T
 import ProgCon.API qualified as API
 import ProgCon.Parser qualified as Parser
+import RIO
+import SimpleCmdArgs
 
 main :: IO ()
 main = do
-  hSetBuffering stdout NoBuffering
-  mainMain
+    hSetBuffering stdout NoBuffering
+    mainMain
 
 mainMain :: IO ()
 mainMain =
-  simpleCmdArgs Nothing "progcon" "musical concert" $
-  subcommands
-  [ Subcommand "communicate" "send a message" $
-    mainCommunicate
-    <$> (T.pack <$> strArg "MESSAGE"),
-    Subcommand "parse" "parse a message" $
-    mainParse
-    <$> (T.pack <$> strArg "MESSAGE")
-
-  ]
+    simpleCmdArgs Nothing "progcon" "musical concert" $
+        subcommands
+            [ Subcommand "communicate" "send a message" $
+                mainCommunicate
+                    <$> (T.pack <$> strArg "MESSAGE")
+            , Subcommand "query" "encode and send a string message" $
+                mainQuery
+                    <$> (T.pack <$> strArg "MESSAGE")
+            , Subcommand "encode" "encode a message" $
+                mainEncode
+                    <$> (T.pack <$> strArg "STRING")
+            , Subcommand "parse" "parse a message" $
+                mainParse
+                    <$> (T.pack <$> strArg "MESSAGE")
+            ]
 
 mainCommunicate :: Text -> IO ()
 mainCommunicate message = do
-  print =<< API.communicate message
+    T.putStrLn =<< API.communicate message
 
 mainParse :: Text -> IO ()
-mainParse message = do
-  print $ Parser.parseExpr message
+mainParse message = case Parser.parseExpr message of
+    Left err -> error err
+    Right expr -> case expr of
+        Parser.EStr txt -> T.putStrLn txt
+        _ -> print expr
+
+mainEncode :: Text -> IO ()
+mainEncode message = T.putStrLn (Parser.encodeString message)
+
+mainQuery :: Text -> IO ()
+mainQuery message = do
+    mainParse =<< API.communicate (Parser.encodeString message)
--- src/ProgCon/Parser.hs
+++ src/ProgCon/Parser.hs
@@ -5,6 +5,7 @@
 module ProgCon.Parser where
 
 import Data.Attoparsec.Text qualified as P
+import Data.List (elemIndex)
 import Data.Text qualified as T
 import RIO
 
@@ -26,5 +27,12 @@ stringP :: P.Parser Text
 stringP = T.map convertChars <$> takeString
   where
     takeString = P.takeWhile (\c -> c >= '!' && c <= '~')
-    convertChars c = order !! (fromEnum c - 33)
-    order = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n"
+    convertChars c = charOrder !! (fromEnum c - 33)
+
+charOrder :: String
+charOrder = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n"
+
+encodeString :: Text -> Text
+encodeString message = T.cons 'S' $ T.map convertChars message
+  where
+    convertChars c = toEnum $ 33 + fromMaybe 0 (elemIndex c charOrder)
