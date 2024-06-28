c79a0a37108f52013fc258e628d3d12edad1203e
Ignat Insarov <kindaro@gmail.com>
2024-06-29 00:58:44 +0800

    Refactor `API`.
    We want to have raw request and response body available so that we can
    test all things in isolation.

--- src/ProgCon/API.hs
+++ src/ProgCon/API.hs
@@ -1,8 +1,11 @@
 module ProgCon.API where
 
-import Data.ByteString (fromStrict, toStrict)
-import Data.Text (Text, pack)
-import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import RIO
+import RIO.ByteString (toStrict)
+import qualified RIO.ByteString.Lazy as Lazy
+import RIO.Text qualified as Text
+
+import Data.Text.Encoding (decodeUtf8)
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import System.Environment
@@ -12,19 +15,26 @@ import Network.HTTP.Types (Status (..))
 apiServer :: String
 apiServer = "https://boundvariable.space/communicate"
 
-communicate :: Text -> IO Text
-communicate body = do
+postBytes ∷ Lazy.ByteString → IO Lazy.ByteString
+postBytes body = do
   manager <- newTlsManager
   token <- getEnv "ICFP_TOKEN"
+  debug ← lookupEnv "ICFP_DEBUG"
   initialRequest <- parseRequest apiServer
   let request =
         initialRequest
           { method = "POST"
-          , requestBody = RequestBodyLBS $ fromStrict $ encodeUtf8 body
+          , requestBody = RequestBodyLBS $ body
           , requestHeaders =
-              [ ("Authorization", "Bearer " <> encodeUtf8 (pack token))
+              [ ("Authorization", "Bearer " <> encodeUtf8 (Text.pack token))
               ]
           }
   response <- httpLbs request manager
-  putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
-  pure $ decodeUtf8 (toStrict $ responseBody response)
+  when (isJust debug) do
+    putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
+  pure $ responseBody response
+
+communicate :: Text -> IO Text
+communicate message = do
+  response ← (postBytes . Lazy.fromStrict . encodeUtf8) message
+  (pure . decodeUtf8 . toStrict) response
