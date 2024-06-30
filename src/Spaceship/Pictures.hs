13814ea0bf599b4ec3eca1c5f5d58e8a394070c0
Tristan Cacqueray <tdecacqu@redhat.com>
2024-06-30 06:50:37 -0400

    Draw path

--- src/Spaceship/Pictures.hs
+++ src/Spaceship/Pictures.hs
@@ -5,7 +5,9 @@ import Graphics.Gloss
 import Spaceship
 
 drawSpaceship :: IVec2 -> [Target] -> Picture
-drawSpaceship pos targets = Pictures $ drawDot green pos : map (drawDot orange) targets
+drawSpaceship pos targets = Pictures (Line path : drawDot green pos : map (drawDot black) targets)
  where
+  path = map mkPoint (pos:targets)
+  mkPoint p = (fromIntegral p.x, fromIntegral p.y)
   drawDot col coord =
-    Translate (fromIntegral coord.x) (fromIntegral coord.y) $ Color col $ circleSolid 1
+    Translate (fromIntegral coord.x) (fromIntegral coord.y) $ Color col $ circleSolid 0.2
